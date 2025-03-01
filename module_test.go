c36bfdbd3d8634b053dbafeb8bdeb2c5a13cf46b
Ren0503 <illuminati0440@gmail.com>
2025-03-01 12:07:00 +0700

    feat: sync up tinh tinh v2

--- module_test.go
+++ module_test.go
@@ -1,4 +1,4 @@
-package ai
+package ai_test
 
 import (
 	"context"
@@ -10,16 +10,17 @@ import (
 
 	"github.com/google/generative-ai-go/genai"
 	"github.com/stretchr/testify/require"
-	"github.com/tinh-tinh/tinhtinh/core"
+	ai "github.com/tinh-tinh/genai/v2"
+	"github.com/tinh-tinh/tinhtinh/v2/core"
 	"google.golang.org/api/option"
 )
 
 func Test_Module(t *testing.T) {
-	userController := func(module *core.DynamicModule) *core.DynamicController {
+	userController := func(module core.Module) core.Controller {
 		ctrl := module.NewController("users")
 
 		ctrl.Get("", func(ctx core.Ctx) error {
-			model := InjectModel(module, "gemini-1.5-flash")
+			model := ai.InjectModel(module, "gemini-1.5-flash")
 			resp, err := model.GenerateContent(context.Background(), genai.Text("Write a story about a magic backpack."))
 			if err != nil {
 				fmt.Println(err)
@@ -32,21 +33,21 @@ func Test_Module(t *testing.T) {
 		return ctrl
 	}
 
-	userModule := func(module *core.DynamicModule) *core.DynamicModule {
+	userModule := func(module core.Module) core.Module {
 		return module.New(core.NewModuleOptions{
-			Imports: []core.Module{
-				ForFeature("gemini-1.5-flash"),
+			Imports: []core.Modules{
+				ai.ForFeature("gemini-1.5-flash"),
 			},
-			Controllers: []core.Controller{
+			Controllers: []core.Controllers{
 				userController,
 			},
 		})
 	}
 
-	appModule := func() *core.DynamicModule {
+	appModule := func() core.Module {
 		return core.NewModule(core.NewModuleOptions{
-			Imports: []core.Module{
-				ForRoot(option.WithAPIKey(os.Getenv("API_KEY"))),
+			Imports: []core.Modules{
+				ai.ForRoot(option.WithAPIKey(os.Getenv("API_KEY"))),
 				userModule,
 			},
 		})
